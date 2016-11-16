b5812f0d6499f1107088dbbe031d21abccacfb8f
ligaowei <su3188@163.com>
2016-11-16 23:48:21 +0800

    first

---
+++ LiGaoWei-Lodash.js
@@ -0,0 +1,13 @@
+var LiGaoWei = {
+  function chunk(arr, n) {
+    var x = Math.ceil(arr.length / n)
+    var result = new Array(x);
+    for (var i = 0; i < x; i++) {
+      result[i] = []
+    }
+    for (var j = 0; j < arr.length; j++) {
+      result[parseInt(j / n)][j % n] = arr[j]
+    }
+    return result
+  },
+}
